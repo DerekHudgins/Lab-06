8011fa9ab1bdff3032320a184483e808a6287aa4
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-15 13:30:34 -0700

    added data

---
+++ data.js
@@ -0,0 +1,49 @@
+const music = {
+    'count': 10,
+    'page':1,
+    'perPage':20,
+    'results':[
+        {
+            id: 1,
+            name: 'The Smiths',
+            description: 'A new wave/emo band from the UK',
+            category: 'newwave',
+            price: 15.00
+        },
+        {
+            id: 2,
+            name: 'The Growlers',
+            description: 'surf rock',
+            category: 'beachgoth',
+            price: 15.00
+        },
+        {
+            id: 3,
+            name: 'Miles Davis',
+            description: 'Jazz from the 1950s',
+            category: 'Jazz',
+            price: 15.00
+        },
+        {
+            id: 4,
+            name: 'Streetlight Manifesto',
+            description: 'high energy ska',
+            category: 'ska/punk',
+            price: 15.00
+        },
+        {
+            id: 5,
+            name: 'The Specials',
+            description: 'first wave ska',
+            category: 'ska',
+            price: 15.00
+        },
+        {},
+        {},
+        {},
+        {},
+        {},
+
+
+    ] 
+}
